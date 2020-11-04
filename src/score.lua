fe565a2fc7c4008dc1a686900391e04457679ec8
jcanovas <jcanovas@edu.tecnocampus.cat>
2020-11-04 16:42:23 +0100

    Funciones basicas para guardar, modificar  y cargar el score

---
+++ src/score.lua
@@ -0,0 +1,31 @@
+local Object = Object or require "lib.classic"
+local w, h = love.graphics.getDimensions()
+local credits = 0
+local loadedCredits = 0
+local file = io.open("savegame.txt", "r+")
+
+function Score:new(last)
+  io.input(file)
+end
+
+function Score:update(dt)
+  
+end
+
+function Score:draw()
+  
+end
+
+function Score:loadCredits() --Loads player credits from a txt file
+  self.loadedCredits = io.read()
+end
+
+function Score:increaseCredits(moarCredits)
+  self.credits = self.credits + moarCredits --Increases player credits by input
+end
+
+function Score:saveCredits() --Saves player credits on a txt file
+  io.write(self.credits)
+end
+
+return Score
