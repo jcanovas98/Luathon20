c2fc5e9a6308eb158a3058d075f260c72a007bdf
jcanovas <jcanovas@edu.tecnocampus.cat>
2020-11-05 23:34:09 +0100

    SE FINITI V1.0

--- src/hud.lua
+++ src/hud.lua
@@ -2,12 +2,26 @@ local Object = Object or require "lib.classic"
 local Vector = Vector or require "src/vector"
 local Hud = Object:extend()
 local w, h = love.graphics.getDimensions(5)
-local file = io.open("savegame.txt", "r+")
+--local file = io.open("savegame.txt", "r+")
 local score
 local loadScore
 
+
+
+
+
 function Hud:new(health)
-  io.input(file)
+ -- io.input(file)
+  self.saveGame = {}
+  self.file = "savegame.txt"
+  f = io.open("savegame.txt")
+  for line in io.lines("savegame.txt") do 
+    self.saveGame[#self.saveGame+1] = line
+  end
+  io.close(f)
+
+  loadScore = self.saveGame[1]
+  print(loadScore)
   
   self.heartList = {}
   self.health = health
@@ -17,8 +31,6 @@ function Hud:new(health)
   end
   
   self.score = 0
-  self.loadScore = io.read() or 0
-  io.close()
   
   font = love.graphics.newFont("pong.ttf",100)
   love.graphics.setFont(font)
@@ -31,7 +43,9 @@ function Hud:update(dt)
   self.lastHealth = self.health
   self.score = self.score + dt * 3
   
-  self:saveScore()
+  if self.score > tonumber(loadScore) then
+    self:saveScore()
+  end
 end
 
 function Hud:draw()
@@ -40,17 +54,15 @@ function Hud:draw()
   end
   love.graphics.setColor(1, 0.8, 0)
   love.graphics.print("Score: "..math.floor(self.score), w - 375, 40 ,0,1/2,1/2)
-  love.graphics.print("Record: "..math.floor(self.loadScore), w - 375, 120 ,0,1/2,1/2)
+  love.graphics.print("Record: "..math.floor(loadScore), w - 375, 120 ,0,1/2,1/2)
   love.graphics.setColor(1, 1, 1)
 end
 
 
 function Hud:saveScore() --Saves player credits on a txt file
-  if self.score > self.loadScore then
-    io.input(io.open("savegame.txt", "r+"))
-    io.write(self.score)
-    io.close()
-  end
+  f = assert(io.open(self.file, "w"))
+  f:write(self.score)
+  f:close()
 end
 
 function Hud:getHealth()
